ccf512398872a25223c6be39b1fc075ad07e101f
Baur <opozali72@gmail.com>
2023-02-11 18:58:37 +0700

    new HomWorld

--- Program.cs
+++ Program.cs
@@ -1,40 +1,4 @@
-﻿// Задача 1
-/*
-int DigitCount(int number)
-{
- int count = 0;
- while(number != 0)
-    {
- count++;
- number /= 10;
-    }
- return count;
-}
-Console.Write("Enter number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Number of digits is: " + DigitCount(num));
-*/
-// Задача 2
-// int Factorial(int number)
-// {
-//     int result = 1;
-//     /* for(int i = 1; i <= number; i++)
-//      result *= i; */
-
-//     while (number >  1)
-//       result *= number--;
-//       //  -- *= *=--;
-
-//     return result;
-// }
-
-// Console.Write ("Enter number N:  ");
-// //int numN = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Factorial is:  " + Factorial(Convert.ToInt32(Console.ReadLine())));
-
-
-
-
+﻿
 // Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
 // и возводит число A в натуральную степень B.
 
